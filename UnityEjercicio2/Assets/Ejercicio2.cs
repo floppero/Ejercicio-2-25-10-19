f503698d1723d51ec4d497a3eb24e71315de81fb
joseantoniomoreno <joseantoniomoreno@ceitartessos.com>
2019-10-25 17:05:08 +0200

    ejercicio 2

---
+++ UnityEjercicio2/Assets/Ejercicio2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2 : MonoBehaviour {
+	public string Nombre;
+	public string Profesion;
+	public string Aficion;
+	public int Edad;
+	public int Hijos;
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Jose Antonio es un chaval estudiando en tartessos y que esta intentando programar pero le cuesta un poco, le gusta jugar a futbol y consiguio algunos bueno trofeos. No tiene hijos y poco mas la verdad");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
